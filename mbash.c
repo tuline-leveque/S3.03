a092d2d259d109364c5e4e8f706c8548a4dc7038
tuline-leveque <tuline.leveque6@etu.univ-lorraine.fr>
2025-01-14 14:51:59 +0100

    lancerCommandeListe

--- mbash.c
+++ mbash.c
@@ -4,30 +4,33 @@
 #include <unistd.h>
 #include <assert.h>
 #define MAXLI 2048
+
+//déclaration des variables
 char cmd[MAXLI];
 char path[MAXLI];
 char rep[MAXLI];
 char* motsCles[] = {"exit\n", "quitter\n", "pwd\n"};
 int rester = 1;
 int pathidx;
+
+//déclaration des fonctions
 void mbash();
 void quitter();
 int estMotCle(char* mot);
 char* getRepertoireCourant();
+void lancerCommandeListe(char* mot);
+
+
 
 int main(int argc, char** argv) {
   while (rester) {
-    printf("Commande: ");
+    printf("\n");
+    printf("%s", getRepertoireCourant());
+    printf(" : ");
     fgets(cmd, MAXLI, stdin);
 
     if(estMotCle(cmd)){
-      if (strcmp(cmd,"exit\n") == 0||strcmp(cmd,"quitter\n") == 0) {
-        quitter();
-      }
-
-      if (strcmp(cmd,"pwd\n") == 0) {
-        getRepertoireCourant();
-      }      
+      lancerCommandeListe(cmd);
     } else {
       printf("truc\n");
       mbash(cmd);
@@ -35,6 +38,7 @@ int main(int argc, char** argv) {
   }
   return 0;
 }
+
 
 void mbash() {
   printf("Execute: %s", cmd);
@@ -50,11 +54,11 @@ void quitter() {
 char* getRepertoireCourant() {
     if (strlen(rep) == 0) {
         getcwd(rep, MAXLI);
-        printf("%s",rep);
     }
     return rep;
 }
 
+//méthode qui permet de savoir si une commande entrée fait partie de la liste des mots clefs (motsClefs) ou non
 int estMotCle(char* mot){
   int size = sizeof(motsCles)/sizeof(char*);
   for(int i = 0; i < size; i++){
@@ -63,4 +67,14 @@ int estMotCle(char* mot){
     }
   }
   return 0;
+}
+
+//méthode qui permet de lancer une commande de la liste de commandes
+void lancerCommandeListe(char* mot) {
+    if ((strcmp(mot,"quitter\n") == 0)||(strcmp(mot,"exit\n")) == 0) {
+        quitter();
+    }
+    else if (strcmp(mot,"pwd\n") == 0) {
+        printf("%s", getRepertoireCourant());
+    }
 }
