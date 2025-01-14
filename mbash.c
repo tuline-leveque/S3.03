ac7ed8d04705bc5d60535b62afbe0afbd027820f
GabinM <gabin.mathieu54@gmail.com>
2025-01-14 14:23:23 +0100

    ajout fonction estMotCle

--- mbash.c
+++ mbash.c
@@ -6,27 +6,32 @@
 #define MAXLI 2048
 char cmd[MAXLI];
 char path[MAXLI];
+char rep[MAXLI];
+char* motsCles[] = {"exit\n", "quitter\n", "pwd\n"};
+int rester = 1;
 int pathidx;
 void mbash();
 void quitter();
+int estMotCle(char* mot);
 char* getRepertoireCourant();
-char rep[MAXLI];
-int rester = 1;
 
 int main(int argc, char** argv) {
   while (rester) {
     printf("Commande: ");
     fgets(cmd, MAXLI, stdin);
-    mbash(cmd);
 
-    if (strcmp(cmd,"exit\n")||strcmp(cmd,"quitter\n")) {
+    if(estMotCle(cmd)){
+      if (strcmp(cmd,"exit\n") == 0||strcmp(cmd,"quitter\n") == 0) {
         quitter();
-    }
+      }
 
-    if (strcmp(cmd,"pwd\n")) {
+      if (strcmp(cmd,"pwd\n") == 0) {
         getRepertoireCourant();
+      }      
+    } else {
+      printf("truc\n");
+      mbash(cmd);
     }
-
   }
   return 0;
 }
@@ -48,4 +53,14 @@ char* getRepertoireCourant() {
         printf("%s",rep);
     }
     return rep;
+}
+
+int estMotCle(char* mot){
+  int size = sizeof(motsCles)/sizeof(char*);
+  for(int i = 0; i < size; i++){
+    if(strcmp(mot, motsCles[i]) == 0){
+      return 1;
+    }
+  }
+  return 0;
 }
