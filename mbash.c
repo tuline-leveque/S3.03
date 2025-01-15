0eb496c6283fdb54a6a1b3123314090dd7fd63b2
GabinM <gabin.mathieu54@gmail.com>
2025-01-15 16:27:56 +0100

    f

--- mbash.c
+++ mbash.c
@@ -29,15 +29,15 @@ void stringSlicer(char* string, char* result[MAXNBSTR]);
 void stringSlicerPutIntoList(char* mot, char* liste[MAXNBSTR], int index);
 
 int main(int argc, char** argv) {
-  printf("##################################################\n");
-  printf("#                                                #\n");
-  printf("#   M     M   BBBBB    AAAAA   SSSSS   H   H     #\n");
-  printf("#   MM   MM   B    B  A     A  S       H   H     #\n");
-  printf("#   M M M M   BBBBB   AAAAAAA  SSSSS   HHHHH     #\n");
-  printf("#   M  M  M   B    B  A     A      S   H   H     #\n");
-  printf("#   M     M   BBBBB   A     A  SSSSS   H   H     #\n");
-  printf("#                                                #\n");
-  printf("##################################################\n");
+  printf("#######################################################\n");
+  printf("#                                                     #\n");
+  printf("#   |M     |M   |BBBBB    |AAAAA   |SSSSS   |H  |H    #\n");
+  printf("#   |MM   |MM   |B   |B  |A    |A  |S       |H  |H    #\n");
+  printf("#   |M|M |M|M   |BBBBB   |AAAAAAA  |SSSSS   |HHHHH    #\n");
+  printf("#   |M  |M |M   |B   |B  |A    |A      |S   |H  |H    #\n");
+  printf("#   |M     |M   |BBBBB   |A    |A  |SSSSS   |H  |H    #\n");
+  printf("#                                                     #\n");
+  printf("#######################################################\n");
 
   while (rester) {
     printf("\n");
